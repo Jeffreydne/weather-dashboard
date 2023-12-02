7d2d02813ab1a66fe374cbb9e9f278b39c562e39
Jeffreydne <Jeffreydne@gmail.com>
2023-12-02 10:34:28 -0800

    initial CSS html header&currentWeather setup & initial js for API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,23 @@
 console.log("I work");
-// access token = 3c174ae84960a3091b2facf671fb569b 
-// for account jeff777AtUCB using jdnelson@berkeley.edu
-let requestUrl = 'https://';
+// DOM variables
+const currentWeatherDiv= document.querySelector("#currentWeather");
+// other variables
+// accsess token for account jeff777AtUCB using jdnelson@berkeley.edu
+const apiKey = '3c174ae84960a3091b2facf671fb569b';
+let city;
+
+// https://api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}
+
+// let requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent('San Francisco')},CA,US&appid=3c174ae84960a3091b2facf671fb569b`;
+
+let requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=San Francisco,ca,us&appid=3c174ae84960a3091b2facf671fb569b`
+
+// let requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=Sacramento,ca,us&appid=3c174ae84960a3091b2facf671fb569b`;
 fetch(requestUrl)
 .then(function (response) {
   return response.json();
 })
 .then(function (data) {
-  console.log(data);
+    let cityData = data
+  console.log(cityData);
 });
