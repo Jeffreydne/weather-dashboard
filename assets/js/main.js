cafbb16ebacaa6492e194f792f8ac4c5a74d61a9
Jeffreydne <Jeffreydne@gmail.com>
2023-12-02 11:56:44 -0800

    add currentWeatherFxn to insert data into currentWeather div

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 console.log("I work");
 // DOM variables
-const currentWeatherDiv= document.querySelector("#currentWeather");
+const currentWeatherDiv = document.querySelector("#currentWeather");
 // other variables
 // accsess token for account jeff777AtUCB using jdnelson@berkeley.edu
 const apiKey = '3c174ae84960a3091b2facf671fb569b';
@@ -13,6 +13,8 @@ let city;
 let requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=San Francisco,ca,us&appid=3c174ae84960a3091b2facf671fb569b`
 
 // let requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=Sacramento,ca,us&appid=3c174ae84960a3091b2facf671fb569b`;
+
+// fetch to api.openweathermap.org
 fetch(requestUrl)
 .then(function (response) {
   return response.json();
@@ -21,3 +23,22 @@ fetch(requestUrl)
     let cityData = data
   console.log(cityData);
 });
+
+//functions
+const currentWeatherFxn = (city, temp, wind, humid) => {
+    const newHead = document.createElement("h2");
+    const newTemp = document.createElement("p");
+    const newWind = document.createElement("p");
+    const newHumid = document.createElement("p");
+    
+    newHead.textContent = city; 
+    newTemp.textContent = temp; 
+    newWind.textContent = wind; 
+    newHumid.textContent = humid; 
+
+    currentWeatherDiv.append(newHead);
+    currentWeatherDiv.append(newTemp);
+    currentWeatherDiv.append(newWind);
+    currentWeatherDiv.append(newHumid);
+}
+currentWeatherFxn("San Francisco", 47, 5, 95);
